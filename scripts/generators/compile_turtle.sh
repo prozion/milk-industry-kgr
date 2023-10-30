07838251f50473f6811f9a00feaa5fb2d348b785
Denis Shirshov <denis.shirshov@gmail.com>
2023-10-31 00:38:27 +0300

    3.4

--- scripts/generators/compile_turtle.sh
+++ scripts/generators/compile_turtle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-compile-ttl.rkt "/home/denis/projects/milk_industry_kgr/source/main_geo.tree" "/home/denis/projects/milk_industry_kgr/turtle/milk_industry_geo.ttl"
+compile-ttl.rkt "/home/denis/projects/milk_industry_kgr/source/main.tree" "/home/denis/projects/milk_industry_kgr/turtle/milk_industry.ttl"
